120e3b984ee67792580f7fad7ba985170c8e5705
Kamal Uddin <37912791+kamal21087@users.noreply.github.com>
2025-01-13 22:14:40 -0500

    Comment out for debugging release

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import Auth from '../../utils/auth';
 import '../../App.css'; // Corrected path
